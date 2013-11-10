bdecb8ae519f4e2be81ce44a60f97420b44964b6
Vin Gardner <vcrgardner@gmail.com>
2013-11-10 18:28:24 +0000

    Shows list of dynamic to-do items.

--- ios/Taskquare/Taskquare/TASTaskListViewController.m
+++ ios/Taskquare/Taskquare/TASTaskListViewController.m
@@ -65,22 +65,23 @@
 {
 #warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
 #warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    return [self.taskItems count];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    static NSString *CellIdentifier = @"Cell";
+    static NSString *CellIdentifier = @"TaskItemCell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
     
-    // Configure the cell...
+    TASTaskItem *toDoItem = [self.taskItems objectAtIndex:indexPath.row];
+    cell.textLabel.text = toDoItem.itemName;
     
     return cell;
 }

57ff8b430e2fe6ad6976c8ee65d145382f78bb24
Vin Gardner <vcrgardner@gmail.com>
2014-01-29 22:29:11 +0000

    Android init.

---
+++ android/taskquare/gen/com/charcade/taskquare/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.charcade.taskquare;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ android/taskquare/src/com/charcade/taskquare/taskListActivity.java
@@ -0,0 +1,81 @@
+package com.charcade.taskquare;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+
+
+/**
+ * An activity representing a list of tasks. This activity
+ * has different presentations for handset and tablet-size devices. On
+ * handsets, the activity presents a list of items, which when touched,
+ * lead to a {@link taskDetailActivity} representing
+ * item details. On tablets, the activity presents the list of items and
+ * item details side-by-side using two vertical panes.
+ * <p>
+ * The activity makes heavy use of fragments. The list of items is a
+ * {@link taskListFragment} and the item details
+ * (if present) is a {@link taskDetailFragment}.
+ * <p>
+ * This activity also implements the required
+ * {@link taskListFragment.Callbacks} interface
+ * to listen for item selections.
+ */
+public class taskListActivity extends FragmentActivity
+        implements taskListFragment.Callbacks {
+
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private boolean mTwoPane;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task_list);
+
+        if (findViewById(R.id.task_detail_container) != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-large and
+            // res/values-sw600dp). If this view is present, then the
+            // activity should be in two-pane mode.
+            mTwoPane = true;
+
+            // In two-pane mode, list items should be given the
+            // 'activated' state when touched.
+            ((taskListFragment) getSupportFragmentManager()
+                    .findFragmentById(R.id.task_list))
+                    .setActivateOnItemClick(true);
+        }
+
+        // TODO: If exposing deep links into your app, handle intents here.
+    }
+
+    /**
+     * Callback method from {@link taskListFragment.Callbacks}
+     * indicating that the item with the given ID was selected.
+     */
+    @Override
+    public void onItemSelected(String id) {
+        if (mTwoPane) {
+            // In two-pane mode, show the detail view in this activity by
+            // adding or replacing the detail fragment using a
+            // fragment transaction.
+            Bundle arguments = new Bundle();
+            arguments.putString(taskDetailFragment.ARG_ITEM_ID, id);
+            taskDetailFragment fragment = new taskDetailFragment();
+            fragment.setArguments(arguments);
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.task_detail_container, fragment)
+                    .commit();
+
+        } else {
+            // In single-pane mode, simply start the detail activity
+            // for the selected item ID.
+            Intent detailIntent = new Intent(this, taskDetailActivity.class);
+            detailIntent.putExtra(taskDetailFragment.ARG_ITEM_ID, id);
+            startActivity(detailIntent);
+        }
+    }
+}
